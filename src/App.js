e1fbc4fed23fa90b09e011aff98e7f68c74800ee
Isaiah <irodol@uw.edu>
2025-01-15 16:22:29 -0800

    Working tip calculator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+// import './App.css';
 
 function App() {
+  const [billInitialCost, setbillInitialCost] = useState(100);
+  const [howMuchYouEnjoyed, setHowMuchYouEnjoyed] = useState(20); // as percentage of tip
+  const [howMuchFriendEnjoyed, setHowMuchFriendEnjoyed] = useState(20);
+
+  const tip = roundToNearestCent(
+    parseFloat(billInitialCost) *
+    ((parseFloat(howMuchYouEnjoyed) + parseFloat(howMuchFriendEnjoyed)) / 2 / 100)
+  );
+
+  function roundToNearestCent(number) {
+    return Math.round(number * 100) / 100;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>How much did you pay?</p>
+      <input value={billInitialCost} onChange={e => setbillInitialCost(e.target.value)}></input>
+      <ServiceEnjoyment howMuchEnjoyed={howMuchYouEnjoyed} setHowMuchEnjoyed={setHowMuchYouEnjoyed}>you</ServiceEnjoyment>
+      <ServiceEnjoyment howMuchEnjoyed={howMuchFriendEnjoyed} setHowMuchEnjoyed={setHowMuchFriendEnjoyed}>you</ServiceEnjoyment>
+      <h3>You pay ${Number(billInitialCost) + tip} (${billInitialCost} + ${tip})</h3>
+    </>
+  );
+}
+
+function ServiceEnjoyment({ howMuchEnjoyed, setHowMuchEnjoyed, children }) {
+  return (
+    <>
+      <p>How much did {children} enjoy?</p>
+      <select value={howMuchEnjoyed} onChange={e => setHowMuchEnjoyed(e.target.value)}>
+        <option value="10">It was good (10%)</option>
+        <option value="15">I really liked it (15%)</option>
+        <option value="20">Absolutely amazing! (20%)</option>
+      </select>
+    </>
   );
 }
 
